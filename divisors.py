2fb130cdfdc77a26bff7241f7d858394519e0e9d
Vinícius Pavão <viniciuspavao2004@gmail.com>
2023-06-16 21:55:08 -0400

    Update divisors.py

--- divisors.py
+++ divisors.py
@@ -5,11 +5,12 @@ def s(x):
 			l.append(i)
 	return sum(l)
 
-def iter(x):
+def iter(x, limit):						# Itera sobre o numero x
+										# se limit = 0, itera sem limites
 	i=1
 	xi = x
 	print("--> n = {:d}".format(x))
-	while(x != 0):
+	while(x != 0 and i < limit):
 		x = s(x)
 		print("[{:d}] : {:d}".format(i, x))
 		if x == s(x):
@@ -18,21 +19,18 @@ def iter(x):
 	return xi
 
 # 276, 552, 564, 660, and 966.
-#alg(8400)
+iter(276, 0)
 #alg(552, 100)
 #alg(564, 100)
 #alg(660, 100)
 #alg(966, 100)
 
 # Caso interessante: volta em 601
-#s(600)
+#iter(600)
 
 # Caso interessante: Numero sociavel
-#alg(12496)
-#dk(576)
+
+#iter(576)
 
 #for i in range(1, 100):
 #	if iter(i) != 0: print("{:d} -> {:d}".format(i, iter(i)))
-
-iter(95)
-iter(27)

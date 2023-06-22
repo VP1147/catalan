2da8ab4d2ed3f4de3acb75d7cb8ba17a4f2003c4
Vinícius Pavão <viniciuspavao2004@gmail.com>
2023-06-22 15:59:32 -0400

    Update basel.py

--- basel.py
+++ basel.py
@@ -6,9 +6,9 @@ def σ(x):
 	σ = sum(i for i in range(1, x+1) if x % i == 0)
 	return σ
 
-n = 1000
+n = 10000
 S = 0
-d = 28
 
 for i in range(1, n):
-	print("[{:d}]	σ(n)/n = {:.2f}		S/n = {:.10f}".format(i, σ(i)/i, S/i))
+	S += σ(i)/i
+	print("[{:d}]	σ(n)/n = {:.2f}		S/n = {:.10f} pi ~ {:.8f}".format(i, σ(i)/i, S/i, (6*S/i)**(1/2)))

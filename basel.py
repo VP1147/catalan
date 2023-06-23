e1f667a9b8d41ef57011356505e745fc83393114
Vinícius Pavão <viniciuspavao2004@gmail.com>
2023-06-23 12:00:32 -0400

    Update basel.py

--- basel.py
+++ basel.py
@@ -1,3 +1,5 @@
+import math as m
+
 def s(x):
 	s = sum(i for i in range(1, x) if x % i == 0)
 	return s
@@ -6,9 +8,23 @@ def σ(x):
 	σ = sum(i for i in range(1, x+1) if x % i == 0)
 	return σ
 
-n = 1000
-S = 0
-d = 28
+def basel(n):
+	S = 0
+	for i in range(1, n+1):
+		S += σ(i)/i
+	print("[{:d}]	σ(n)/n = {:.2f}		S/n = {:.16f}	S > {:d}".format(i, σ(i)/i, S/i, int(S)))
+	print("--- Final ---")
 
-for i in range(1, n):
-	print("[{:d}]	σ(n)/n = {:.2f}		S/n = {:.10f}".format(i, σ(i)/i, S/i))
+def basel_simulation(n):
+	basel = m.pi**2 / 6
+	print("basel = {:.10f}".format(basel))
+	print("S ~ {:d}".format(int(basel * n)))
+
+n = 10000
+
+basel(n)
+
+basel_simulation(n)
+
+# Imprimindo números 	: 0m15,675s
+# Sem imprimir 			: 0m3,487s

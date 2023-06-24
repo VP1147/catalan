a9162ae38f761e7e14b97e70ef6f58c30bab453b
Vinícius Pavão <viniciuspavao2004@gmail.com>
2023-06-23 21:21:20 -0400

    Create basel.c

---
+++ basel.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <math.h>
+
+int s(int x) {
+    int sum = 0;
+    for (int i = 1; i < x; i++) {
+        if (x % i == 0) {
+            sum += i;
+        }
+    }
+    return sum;
+}
+
+int sigma(int x) {
+    int sum = 0;
+    for (int i = 1; i <= x; i++) {
+        if (x % i == 0) {
+            sum += i;
+        }
+    }
+    return sum;
+}
+
+int main() {
+    int n = 100000;
+    double S = 0.0;
+    int i;
+    for (i = 1; i < n; i++) {
+        S += (double)sigma(i) / i;
+    }
+    printf("[%d]\tσ(n)/n = %.2f\t\tS/n = %.10f\tpi ~ %.8f\n", i, (double)sigma(i) / i, S / i, sqrt(6 * S / i));
+    return 0;
+}
+
+// gcc basel.c -o basel -lm

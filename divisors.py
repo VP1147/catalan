8eb10c1d6bf03cfcb2bf59f602af90a42ce1ea8c
Vinícius Pavão <viniciuspavao2004@gmail.com>
2023-07-06 22:45:01 -0400

    Update divisors.py

--- divisors.py
+++ divisors.py
@@ -5,32 +5,57 @@ def s(x):
 			l.append(i)
 	return sum(l)
 
-def iter(x, limit):						# Itera sobre o numero x
-										# se limit = 0, itera sem limites
-	i=1
-	xi = x
-	print("--> n = {:d}".format(x))
-	while(x != 0 and i < limit):
-		x = s(x)
-		print("[{:d}] : {:d}".format(i, x))
-		if x == s(x):
-			return x
+def sigma(x):
+	return s(x) + x
+
+def t(x):				# H. J. J. te Riele
+						# A NOTE ON THE CATALAN-DICKSON CONJECTURE
+	return 2*s(x) + x
+
+def iter(n):						# Itera sobre o numero x
+	i = 0
+	while(n != 0):
+		print("[{:d}] : {:d}".format(i, n))
+		n = s(n)
 		i+=1
-	return xi
 
-# 276, 552, 564, 660, and 966.
-iter(276, 0)
-#alg(552, 100)
-#alg(564, 100)
-#alg(660, 100)
-#alg(966, 100)
+def IterList(n):						# Itera sobre o numero x
+										# Retorna uma lista com as somas
+										# em funcao da iteracao.
+	X = []
+	Y = []
+	i = 0
+	while(n != 0):
+		print("[{:d}] : {:d}".format(i, n))
+		X.append(i)
+		Y.append(n)
+		n = s(n)
+		i+=1
+	return X, Y
 
-# Caso interessante: volta em 601
-#iter(600)
+import matplotlib.pyplot as plt
+n = 494
+X, Y = IterList(n)
+fig = plt.figure()
 
-# Caso interessante: Numero sociavel
+plt.plot(X, Y)
+plt.show()
 
-#iter(576)
+## Anotacoes ##
+
+# Lehmer Five: 276, 552, 564, 660, e 966.
+
+# Casos interessantes:
+# 600: volta em 601
+# 1024: volta em 1023
+# 800: converge para 1153 em 1 iteracao
 
 #for i in range(1, 100):
 #	if iter(i) != 0: print("{:d} -> {:d}".format(i, iter(i)))
+
+# Muitas iteracoes:
+# 234
+# 7868 (Nao eh monot. cresc.)
+
+# Perfeitos:
+# 496
